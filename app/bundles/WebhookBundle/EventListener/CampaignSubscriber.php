cc91ee82ac27a87454e0bd2335686ea3178bccab
Zdeno Kuzmany <zdeno@kuzmany.biz>
2019-02-16 17:30:17 +0100

    Allow html tags in webhook campaign action

--- app/bundles/WebhookBundle/EventListener/CampaignSubscriber.php
+++ app/bundles/WebhookBundle/EventListener/CampaignSubscriber.php
@@ -69,10 +69,11 @@ class CampaignSubscriber extends CommonSubscriber
     public function onCampaignBuild(Events\CampaignBuilderEvent $event)
     {
         $sendWebhookAction = [
-            'label'       => 'mautic.webhook.event.sendwebhook',
-            'description' => 'mautic.webhook.event.sendwebhook_desc',
-            'formType'    => 'campaignevent_sendwebhook',
-            'eventName'   => WebhookEvents::ON_CAMPAIGN_TRIGGER_ACTION,
+            'label'                 => 'mautic.webhook.event.sendwebhook',
+            'description'           => 'mautic.webhook.event.sendwebhook_desc',
+            'formType'              => 'campaignevent_sendwebhook',
+            'formTypeCleanMasks'    => 'clean',
+            'eventName'             => WebhookEvents::ON_CAMPAIGN_TRIGGER_ACTION,
         ];
         $event->addAction('campaign.sendwebhook', $sendWebhookAction);
     }
