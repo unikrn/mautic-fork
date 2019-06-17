85cd2c2fdd1e22a59cafeb11d0ac040d9e43e453
Zdeno Kuzmany <zdeno@kuzmany.biz>
2019-06-17 22:52:55 +0200

    cherry pick from kuzmany/disable-tracking-404

--- app/bundles/PageBundle/Form/Type/ConfigTrackingPageType.php
+++ app/bundles/PageBundle/Form/Type/ConfigTrackingPageType.php
@@ -52,13 +52,24 @@ class ConfigTrackingPageType extends AbstractType
                 ],
             ]
         );
+        $builder->add(
+            'disable_tracking_404',
+            'yesno_button_group',
+            [
+                'label' => 'mautic.page.config.form.disable_tracking_404',
+                'data'  => isset($options['data']['disable_tracking_404']) ? (bool) $options['data']['disable_tracking_404'] : false,
+                'attr'  => [
+                    'tooltip'      => 'mautic.page.config.form.disable_tracking_404.tooltip',
+                ],
+            ]
+        );
 
         $builder->add(
             'track_contact_by_ip',
             'yesno_button_group',
             [
                 'label' => 'mautic.page.config.form.track_contact_by_ip',
-                'data'  => (bool) $options['data']['track_contact_by_ip'],
+                'data'  => isset($options['data']['track_contact_by_ip']) ? (bool) $options['data']['track_contact_by_ip'] : false,
                 'attr'  => [
                     'tooltip'      => 'mautic.page.config.form.track_contact_by_ip.tooltip',
                     'data-show-on' => '{"config_trackingconfig_anonymize_ip_1":"checked"}',
